3bcbcd38977802f7065df57cbb816e667f66e9ac
heetbeet <sfstreicher@gmail.com>
2022-02-08 13:22:52 +0200

    update package numbers

--- upload_to_julia.bat
+++ upload_to_julia.bat
@@ -3,4 +3,4 @@ setlocal
 set "package=SpreadRows"
 cd "%~dp0"
 C:\Users\simon\Juliawin\julia.exe -e "using Pkg; pkg\"dev .\"; using %package%; pth=dirname(pathof(%package%)); cd(pth); using LocalRegistry; using %package%; register(%package%)"
-pause
+if /i "%comspec% /c %~0 " equ "%cmdcmdline:"=%" pause
