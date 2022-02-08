7961d6a720c234b1e2c5fcb4d22f4b811cbc0ff2
heetbeet <sfstreicher@gmail.com>
2022-02-08 14:33:44 +0200

    Added more stuff to try to get package upload up and running again

--- upload-to-package-manager.cmd
+++ upload-to-package-manager.cmd
@@ -2,5 +2,5 @@
 setlocal
 set "package=SpreadRows"
 cd "%~dp0"
-C:\Users\simon\Juliawin\julia.exe -e "using Pkg; pkg\"dev .\"; using %package%; pth=dirname(pathof(%package%)); cd(pth); using LocalRegistry; using %package%; register(%package%)"
+"%~dp0..\..\julia\julia.exe" -e "using Pkg; pkg\"dev .\"; using %package%; pth=dirname(pathof(%package%)); println(pth); cd(pth); using LocalRegistry; using %package%; register(%package%)"
 if /i "%comspec% /c %~0 " equ "%cmdcmdline:"=%" pause
